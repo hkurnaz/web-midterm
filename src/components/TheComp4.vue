6ab5397623227c4e5eb1277dbcc62b6d572ebf57
FurokoDShoyo <f.kamer4@gmail.com>
2023-12-13 14:15:19 +0300

    FComp1
    Footer

---
+++ src/components/TheComp4.vue
@@ -0,0 +1,355 @@
+<template >
+  <q-footer elevated style="height: 690px; background-color: #27356f;">
+    <div class="q-pa-md example-row-equal-width">
+      <div class="row " style="margin-left: 360px;margin-right: 360px;">
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim <br> ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Ürün ve Hizmetler</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell Pasaj
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Pasaj Blog
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Pasaj Gaming
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Paketler</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Dijital
+            Servisler
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kampanyalar
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Sosyal Destek
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kulüp ve
+            Programlar</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnternet Hız
+            Testi</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Faturalı
+            Paketler
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="hoverable-text"
+            style="cursor: pointer; margin-bottom: 10px; margin-top: 50px;  text-decoration: none;">Hakkımızda</h6>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İletişim</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Genel Bakış
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Gizlilik Ve
+            Güvenlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Haberler &
+            Duyurular</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell 5G
+          </p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Kurumsal
+            İletişim ve Sürdürülebilirlik</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Yatırımcı
+            İlişkileri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">İnsan
+            Kaynakları</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Afet
+            Tedbirleri</p>
+          <p class="hoverable-text" style="cursor: pointer; margin-bottom: 10px; ;  text-decoration: none;">Turkcell GSYF
+          </p>
+        </div>
+      </div>
+    </div>
+    <hr class="long-line" style=" width: 1180px;">
+    <div style="margin-left: 390px;">
+      <span style="text-decoration:underline ;cursor: pointer;">Türkçe</span>
+      <span class="hoverable-text1  " style="margin-left: 20px; cursor: pointer; ">English</span>
+      <span class="hoverable-text1  " style="margin-left: 20px; cursor: pointer; ">عربى</span>
+      <span class="hoverable-text1  " style="margin-left: 20px; cursor: pointer; ">русский</span>
+      <span style="margin-left: 390px;">Bizi Takip Edin</span>
+      <span class="social-icon" style="margin-left: 115px;" title="Facebook" target="_blank">
+        <i class="fab fa-facebook"></i>
+      </span>
+      <span class="social-icon" style="margin-left: 15px;" title="Instagram" target="_blank">
+        <i class="fab fa-instagram"></i>
+      </span>
+      <span class="social-icon" style="margin-left: 15px;" title="youtube" target="_blank">
+        <i class="fab fa-youtube"></i>
+      </span>
+      <span class="social-icon" style="margin-left: 15px;" title="linkedin" target="_blank">
+        <i class="fab fa-linkedin"></i>
+      </span>
+    </div>
+    <div class="container-p" style="margin-top: 40px;">
+      <div class="o-p-footer__partners">
+
+
+        <div data-component='PasajSlider' id="" class="m-p-slider">
+          <div class="swiper-container">
+            <div class="swiper-wrapper ">
+
+              <div class="swiper-slide" v-for="(item, index) in 1" :key="index">
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/fizy-logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/sol-yeni-logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/platinum-logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/bip-logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/TurkiyeninUygulamalari-Logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/tv-plus-logo-yeni.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/LifeBox-Logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/paycell-logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/gnc-logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+                <span style="cursor: pointer;">
+                  <img class="partner-image"
+                    src="https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/Kategori/menu/Global-Bilgi-Logo.png?17735349480651"
+                    title="Fizy" alt="Fizy"></span>
+
+              </div>
+
+
+            </div>
+          </div>
+          <div class="m-p-slider__nav">
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <hr class="o-p-footer__line">
+    <div>
+      <span class="hoverable-text"
+        style="cursor: pointer; margin-bottom: 10px; margin-left: 380px;  text-decoration: none;">
+        Gizlilik ve
+        Güvenlik</span>
+      <span class="hoverable-text"
+        style="cursor: pointer; margin-bottom: 10px; margin-left: 10px;  text-decoration: none;">
+        Tarife Karşılaştırma</span>
+      <span style="margin-left: 600px;">© 2022 Turkcell</span>
+    </div>
+
+
+  </q-footer>
+</template>
+
+<script setup lang="ts">
+
+
+
+import '@fortawesome/fontawesome-free/css/all.css';
+</script>
+<style scoped>
+.partner-image {
+  transition: filter 0.2s ease-out;
+  filter: brightness(0.7);
+  /* Filter animasyonu ekler, 0.3 saniyede tamamlanır */
+}
+
+.partner-image:hover {
+  filter: brightness(1.7);
+  /* Gölge efektini ekler, 1.2 parlaklık ekler (1.0 normal, daha yüksek değer daha parlak) */
+}
+
+.swiper-wrapper {
+  display: flex;
+  justify-content: center;
+  /* Flexbox kullanarak içerikleri yan yana sırala */
+}
+
+.swiper-slide {
+  flex: 0 0 auto;
+  margin-right: 80px;
+  /* İçeriklerin boyutlarını otomatik belirle, sığdır */
+}
+
+.hoverable-text:hover {
+
+  color: #ffcc00;
+
+}
+
+.hoverable-text1 {
+  color: gray;
+  /* Başlangıçta gri renkte olacak */
+  transition: color 0.1s;
+  /* Geçiş efekti ekleyebilirsiniz */
+}
+
+.hoverable-text1:hover {
+  color: #ffffff;
+  text-decoration: underline;
+}
+
+.social-icon {
+  text-decoration: none;
+  color: #ffffff;
+  /* Başlangıçta beyaz renkte */
+  transition: color 0.1s;
+  /* Rengin değişmesini düzenlemek için bir geçiş efekti */
+}
+
+/* Mouse hover durumunda stil değişiklikleri */
+.social-icon:hover {
+  color: #ffcc00;
+  /* Hover durumunda turuncuya dönecek */
+}
+</style>
+
+
+
